9d8d7b1b19e07fef3942a181e9bbd27de9359ae1
Vaibhav Vesmaker <vaibhavvesmaker@gmail.com>
2025-03-29 03:22:49 -0400

    Configured Vite for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
-  },
+  base: "/Portfolio2025/", // Add this line with your repository name
 });
